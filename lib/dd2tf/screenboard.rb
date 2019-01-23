9bf5336b67d2df9a9c684098cf70c029d37377cd
Gosuke Miyashita <gosukenator@gmail.com>
2019-01-23 18:24:51 +0900

    Implement nested parameters

--- lib/dd2tf/screenboard.rb
+++ lib/dd2tf/screenboard.rb
@@ -42,20 +42,148 @@ module Dd2tf
             else
               key = k
             end
-            if v == true || v == false
-              b[key] = v
-            elsif v.is_a?(Integer)
-              b[key] = v
-            elsif v.is_a?(Array)
-              b[key] = v
+
+            if key == "widgets"
+              b[key] = filter_widgets(v)
+            elsif key == "template_variables"
+              b[key] = filter_template_variables(v) if v != []
             else
-              b[key] = %Q{"#{v}"}
+              b[key] = format_value(v)
             end
           end
         end
       end
 
       b
+    end
+
+    def filter_widgets(widgets)
+      excludes = %w(
+        monitor
+        title
+        params
+        showTitle
+      )
+      
+      keys = {
+        "mustShowErrors" => "must_show_errors",
+        "mustShowBreakdown" => "must_show_breadown",
+        "hideZeroCounds" => "hide_zero_counts",
+        "mustShowResourceList" => "must_show_resource_list",
+        "title_text" => "title",
+        "mustShowHits" => "must_show_hits",
+        "mustShowDistribution" => "must_show_distribution",
+        "mustShowLatency" => "must_show_latency",
+      }
+
+      filtered_widgets = []
+      widgets.each do |widget|
+        w = {}
+        widget.each do |k, v|
+          if !excludes.include?(k)
+            if v != nil && v != ""
+              if keys[k] != nil
+                key = keys[k]
+              else
+                key = k
+              end
+
+              if key == "tile_def"
+                w[key] = filter_tile_def(v)
+              elsif key == "time"
+                w[key] = filter_time(v) if v != {}
+              else
+                w[key] = format_value(v)
+              end
+            end
+          end
+        end        
+        filtered_widgets << w
+      end
+      filtered_widgets
+    end
+
+    def filter_template_variables(v)
+      template_variables = []
+      v.each do |t|
+        template_variable = {}
+        t.each do |k, v|
+          template_variable[k] = format_value(v)
+        end
+        template_variables << template_variable
+      end
+      template_variables
+    end
+    
+    def filter_tile_def(v)
+      keys = {
+        "noMetricHosts" => "no_metric_hosts",
+        "noGroupHosts" => "no_group_hosts",
+      }
+      
+      tile_def = {}
+      v.each do |k, v|
+        if v != "" && v != nil && v != {}
+          if keys[k] != nil
+              key = keys[k]
+          else
+            key = k
+          end
+
+          if key == "requests"
+            tile_def[key] = filter_requests(v)
+          else
+            tile_def[key] = format_value(v)
+          end
+        end
+      end
+      tile_def
+    end
+
+    def filter_requests(v)
+      requests = []
+      v.each do |r|
+        request = {}
+        r.each do |k, v|
+          if k == "conditional_formats"
+            request[k] = filter_conditional_formats(v)
+          elsif v != nil && v != "" && v != [] && v != {}
+            request[k] = format_value(v)
+          end
+        end
+        requests << request
+      end
+      requests
+    end
+
+    def filter_conditional_formats(v)
+      formats = []
+      v.each do |f|
+        format = {}
+        f.each do |k, v|
+          if v != ""
+            format[k] = format_value(v)
+          end
+        end
+        formats << format
+      end
+      formats
+    end
+    
+    def filter_time(v)
+      v
+    end
+    
+    def format_value(v)
+      if v == true || v == false
+        v
+      elsif v.is_a?(Integer)
+        v
+      elsif v.is_a?(Array)
+        v
+      else 
+        %Q{"#{v}"}
+      end
     end
   end
 end
--- lib/templates/screenboard.erb.rb
+++ lib/templates/screenboard.erb.rb
@@ -1,8 +1,55 @@
 resource "datadog_screenboard" "<%= board_name %>" {
   <%- board.each do |k, v| -%>
     <%- if k == "widgets" -%>
+      <%- v.each do |widget| -%>
+      widget {
+          <%- widget.each do |k, v| -%>
+             <%- if k == "tile_def" -%>
+             tile_def {
+                <%- v.each do |k, v| -%>
+                  <%- if k == "requests" -%>
+                    <%- v.each do |request| -%>
+                      request {
+                      <%- request.each do |k, v| -%>
+                         <%- if k == "conditional_formats" -%>
+                           <%- v.each do |format| -%>
+                           conditional_format {
+                             <%- format.each do |k, v| -%>
+                               <%= k %> = <%= v %>
+                             <%- end -%>
+                           }
+                           <%- end -%>
+                         <%- else -%>
+                           <%= k %> = <%= v %>
+                         <%- end -%>
+                      <%- end -%>
+                      }
+                    <%- end -%>
+                  <%- else -%>
+                    <%= k %> = <%= v %>
+                  <%- end -%>
+                <%- end -%>
+             }
+           <%- elsif k == "time" -%>
+           time {
+              <%- v.each do |k, v| -%>
+                <%= k %> = "<%= v %>"
+              <%- end -%>
+           }
+             <%- else -%>
+               <%= k %> = <%= v %>
+             <%- end  -%>
+          <%- end -%>
+      }
+      <%- end -%>
     <%- elsif k == "template_variables" -%>
-      # template_variables
+      <%- v.each do |template| -%>
+      template_variable {
+        <%- template.each do |k, v| -%>
+            <%= k %> = <%= v %>
+        <%- end -%>
+      }
+      <%- end -%>
     <%- else -%>                                     
       <%= k %> = <%= v %>
     <%- end -%>
